278fc7dac7297a2ecf009ee3eaa4291984e47b8c
IvanaTwT <masterpiecetwt@gmail.com>
2024-08-02 14:30:55 -0300

    agregando busqueda

--- src/components/BarraSearch.jsx
+++ src/components/BarraSearch.jsx
@@ -1,18 +1,29 @@
+import "./styles.css"
 function BarraSearch() {
     return (
-        <div className="field has-addons">
-            <div className="control is-expanded">
-                {/* is-expanded */}
-                <input
-                    className="input is-rounded"
-                    type="text"
-                    placeholder="Buscar..."
-                />
+        <div className="search-container">
+            <div >
+                <img className="background-image" src="src/components/fondo.jpg" alt=""/>
             </div>
-            <div className="control ">
-                <button className="button is-info is-medium is-rounded">
-                    <i className="fas fa-search"></i>
-                </button>
+            
+            <div className="">
+                <div className="box search-box" style={{width:"500px"}}>
+                  <h2 className="title">¿Qué te gustaría cocinar?</h2>
+                  <form className="">
+                      {/* <input type="text" placeholder="Buscar..." />
+                      <button type="submit">Buscar</button> */}
+                      <div className="is-flex is-align-items-center is-justify-content-center field has-addons">
+                      <div className="control is-expanded">{/* is-expanded */}
+                        <input className="button input  input-image"  type="text" placeholder="Buscar..." />
+                      </div>
+                      <div className="control ">
+                        <button className="button is-dark is-medium"> {/* is-rounded */}
+                          <i className="fas fa-search"></i>
+                        </button>
+                      </div>
+                    </div>
+                  </form>
+                </div>
             </div>
         </div>
     );
